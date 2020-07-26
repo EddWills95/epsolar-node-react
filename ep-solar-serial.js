27c3af576e28c45cfa4f0c66691252075ad07343
Edd Williams <edd.williams@me.com>
2020-07-26 16:36:01 +0100

    continue refactor

--- ep-solar-serial.js
+++ ep-solar-serial.js
@@ -5,107 +5,110 @@ class EPSolarSerial {
 
     constructor(serialAddress = "/dev/ttyUSB0") {
         this.client = new ModbusRTU();
-        this.serialAddress = serialAddress
+        this.serialAddress = serialAddress;
     }
 
     connect = () => {
-        return this.client.connectRTU(this.serialAddress, { baudRate: 115200 });
-    }
+        return this.client.connectRTU(this.serialAddress, {
+            baudRate: 115200,
+        });
+    };
 
-    getStats = async () => {
-        const stats = [];
+    getBatteryData = async () => {
         const locations = {
-            "Max input voltage today": 0x3300,
-            "Min input voltage today": 0x3301,
             "Max battery voltage today": 0x3302,
             "Min battery voltage today": 0x3303,
-            "Consumed energy today": 0x3304,// -3305
-            "Consumed energy this month": 0x3306, // -3307
-            "Consumed energy this year": 0x3308, // -3309
-            "Total consumed energy": 0x330A, // -330B
-            "Generated energy today": 0x330C, // -330D
-            "Generated energy this moth": 0x330E, // -330F
-            "Generated energy this year": 0x3310, // -3311
-            "Total generated energy": 0x3312, // -3313
-            "Carbon dioxide reduction": 0x3314, // -3315
-            "Net battery current": 0x331B, // -331C
-            "Battery temperature": 0x331D,
-            "Ambient temperature": 0x331E
-        }
+            "Battery voltage": 0x3104,
+            "Battery charging current": 0x3105,
+            "Battery charging power": 0x3106, //-3107,
+            "Battery SOC": 0x311a,
+        };
 
-        try {
-            for (const [name, position] of Object.entries(locations)) {
-                const { data } = await this.client.readInputRegisters(position, 1)
-                stats.push({ [name]: parseInt(data[0]) / 100 })
-            }
-        } catch (error) {
-            console.log(error)
-        }
+        return await this._readRegisters(locations);
+    };
 
-        return stats;
-    }
-
-    getLiveData = async () => {
-        const liveData = [];
+    getSolarData = async () => {
         const locations = {
             "PV array voltage": 0x3100,
             "PV array current": 0x3101,
             "PV array power": 0x3102, // -3103,
-            "Battery voltage": 0x3104,
-            "Battery charging current": 0x3105,
-            "Battery charging power": 0x3106, //-3107,
-            "Load voltage": 0x310C,
-            "Load current": 0x310D,
-            "Load power": 0x310E, //-310F				
-            "Battery temperature": 0x3110,
-            "Charger temperature": 0x3111,
-            "Heat sink temperature": 0x3112,
-            "Battery SOC": 0x311A,
-            "Remote battery temperature": 0x311B,
-            "System rated voltage": 0x311C,
-            "Battery status": 0x3200,
-            "Equipment status": 0x3201,
-        }
+            "Max input voltage today": 0x3300,
+            "Min input voltage today": 0x3301,
+        };
 
-        try {
-            for (const [name, position] of Object.entries(locations)) {
-                const { data } = await this.client.readInputRegisters(position, 1)
-                liveData.push({ [name]: parseInt(data[0]) / 100 })
-            }
-        } catch (error) {
-            console.log(error)
-        }
+        return await this._readRegisters(locations);
+    };
 
-        return liveData;
-    }
+    getGenerationStats = async () => {
+        const locations = {
+            "Generated energy today": 0x330c, // -330D
+            "Generated energy this month": 0x330e, // -330F
+            "Generated energy this year": 0x3310, // -3311
+            "Total generated energy": 0x3312, // -3313
+            "Carbon dioxide reduction": 0x3314, // -3315
+        };
+
+        return await this._readRegisters(locations);
+    };
 
     // Checking the buffer, i'm going to assume: 0 Day, 1 Night
-    getDayNight = async () => {
+    isNight = async () => {
         try {
-            const data = await this.client.readDiscreteInputs(0x200C, 1);
-            console.log(data);
+            const data = await this.client.readDiscreteInputs(0x200c, 1);
             return data.data[0];
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
-    }
+    };
+
+    getData = async () => {
+        const solarData = await this.getSolarData();
+        const batteryData = await this.getBatteryData();
+        const generationStats = await this.getGenerationStats();
+        const isNight = await this.isNight();
+        return { solarData, batteryData, generationStats, isNight };
+    };
+
+    _convertNameToObjectKey = (name) => {
+        return name.toString().toLowerCase().split(" ").join("_");
+    };
+
+    _readRegisters = async (locations) => {
+        const registerData = [];
+        try {
+            for (const [name, position] of Object.entries(locations)) {
+                const { data } = await this.client.readInputRegisters(
+                    position,
+                    1
+                );
+                registerData.push({
+                    label: name,
+                    value: parseInt(data[0]) / 100,
+                    // [this._convertNameToObjectKey(name)]: {
+                    //     label: name,
+                    //     value: parseInt(data[0]) / 100,
+                    // },
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        return registerData;
+    };
 }
 
 module.exports = EPSolarSerial;
 
-const EPSolar = new EPSolarSerial();
+// const EPSolar = new EPSolarSerial();
 
-const getData = async () => {
-    const dayNight = await EPSolar.getDayNight()
+// const getData = async () => {
+//     const solarStats = await EPSolar.getSolarStats();
+//     const batteryData = await EPSolar.getBatteryData();
+//     const generationStats = await EPSolar.getGenerationStats();
+//     return { solarStats, batteryData, generationStats };
+// };
 
-    const liveData = await EPSolar.getLiveData();
-
-    const stats = await EPSolar.getStats();
-
-    console.log({ dayNight, liveData, stats });
-};
-
-EPSolar.connect().then(() => {
-    getData();
-});
-
+// EPSolar.connect().then(() => {
+//     getData();
+// });
