8f1ef321f620605724fcd0bf28f87dc9db569636
EddWills95 <edd.williams@me.com>
2020-07-26 16:40:38 +0100

    update constants url

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,6 @@
-export const socketUrl =
-    `ws://${process.env.REACT_APP_ENDPOINT}:8080` || "ws://localhost:8080";
-export const endpointUrl =
-    `http://${process.env.REACT_APP_ENDPOINT}:8080` || "http://localhost:8080";
+export const socketUrl = `ws://${
+    process.env.REACT_APP_ENDPOINT || "localhost"
+}:8080`;
+export const endpointUrl = `http://${
+    process.env.REACT_APP_ENDPOINT || "localhost"
+}:8080`;
