0c1162e0859c409dcad80ec69c346cf42201ea6b
EddWills95 <edd.williams@me.com>
2020-07-28 20:49:06 +0100

    add some charts

--- client/src/components/battery/index.jsx
+++ client/src/components/battery/index.jsx
@@ -4,30 +4,31 @@ import PropTypes from "prop-types";
 import "./style.scss";
 
 const Battery = ({ state = 0 }) => {
-    const calculateWidth = () => {
-        const newWidth = (state * 100) / 4;
-        return `${newWidth}px`;
-    };
+  const calculateWidth = () => {
+    const newWidth = (state * 100) / 4;
+    return `${newWidth}px`;
+  };
 
-    return (
-        <div className="batteryContainer">
-            <div className="batteryOuter">
-                <div
-                    id="batteryLevel"
-                    style={{ width: calculateWidth() }}
-                ></div>
-            </div>
-            <div className="batteryBump"></div>
-            <span>{state * 100}%</span>
+  return (
+    <div className="batteryContainer">
+      <div className="batteryOuter">
+        <div
+          id="batteryLevel"
+          style={{ width: calculateWidth() }}
+        >
         </div>
-    );
+      </div>
+      <div className="batteryBump"></div>
+      <span>{state * 100}%</span>
+    </div>
+  );
 };
 
 Battery.propTypes = {
-    state: PropTypes.number,
+  state: PropTypes.number,
 };
 
-export default Battery;
+export default React.memo(Battery);
 
 // Need to work out the ratio of 25px as 100%
 // / 4
--- client/src/components/data/index.jsx
+++ client/src/components/data/index.jsx
@@ -1,46 +1,86 @@
 import React, { useState, useEffect } from "react";
-import usePrevious from "../../hooks/use-previous";
+import Chart from "react-apexcharts";
 
-import { labelMapper } from "../../utils/label-mapper";
+const Data = ({ data, title, endpoint }) => {
+  const [chartData, setChartData] = useState(undefined);
+  const [showChart, setShowChart] = useState(false);
 
-const Data = ({ name, value }) => {
-    const [additionalClass, setClass] = useState("");
-    const previousValue = usePrevious(value);
+  const generateSuffix = (label) => {
+    const lowerCaseLabel = label.toLowerCase();
+    if (lowerCaseLabel.includes("voltage")) return "V";
+    if (lowerCaseLabel.includes("power")) return "W";
+    if (lowerCaseLabel.includes("current")) return "A";
+    if (lowerCaseLabel.includes("generated")) return "kW/h";
+  };
 
-    // useEffect(() => {
-    //     if (value !== previousValue) {
-    //         if (value > previousValue) {
-    //             // flash green
-    //             setClass("increase");
-    //         }
+  const handleShowHideData = async () => {
+    if (!chartData) {
+      await fetch(`http://192.168.0.55:8080/${endpoint}`)
+        .then((response) => response.json())
+        .then((json) => json.map((i) => i.value))
+        .then((data) => {
+          setShowChart(true);
+          setChartData(data);
+          return;
+        });
+    }
 
-    //         if (value < previousValue) {
-    //             // flash red;
-    //             setClass("decrease");
-    //         }
+    if (chartData && showChart) {
+      setShowChart(false);
+      return;
+    }
 
-    //         setTimeout(() => {
-    //             setClass("");
-    //         }, 1100);
-    //     }
-    // }, [value, previousValue]);
+    setShowChart(true);
+  };
 
-    const postfix = () => {
-        if (name.includes("current")) return "A";
-        if (name.includes("voltage")) return "V";
-    };
+  const options = {
+    chart: {
+      height: 350,
+      type: "line",
+      zoom: {
+        enabled: false,
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      curve: "straight",
+    },
+    grid: {
+      row: {
+        colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+        opacity: 0.5,
+      },
+    },
+  };
 
-    return (
-        <React.Fragment>
-            <div className={`data ${additionalClass}`}>
-                <h3 className="title">{labelMapper(name)}</h3>
-                <p className="value">
-                    {value}
-                    <span className="value__postfix">{postfix()}</span>
-                </p>
-            </div>
-        </React.Fragment>
-    );
+  return (
+    <div className="data-container">
+      <h2 className="data-container__title">{title}</h2>
+      {data.map((data, index) => (
+        <div className="data-container__data" key={index}>
+          <p>{data.label}</p>
+          <p>
+            {data.value}
+            <span className="data-container__data__suffix">
+              {generateSuffix(data.label)}
+            </span>
+          </p>
+        </div>
+      ))}
+      <button className="data-container__button" onClick={handleShowHideData}>
+        {showChart ? "Hide" : "Show"} data chart
+      </button>
+      {showChart && (
+        <Chart
+          type="line"
+          series={[{ name: title, data: chartData }]}
+          options={options}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Data;
